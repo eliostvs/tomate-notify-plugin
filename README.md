0050de9f432cba135068140eae97c0a934e3bae7
Elio Duarte <eduarte@thoughtworks.com>
2019-01-20 13:26:00 -0200

    Using new event system payload.
    The timer, session and settings now emit a payload object instead of a dictionary

--- README.md
+++ README.md
@@ -17,6 +17,12 @@ Bugs and suggestions should be reported [here](https://github.com/eliostvs/tomat
 Changelog
 ---------
 
+### [Unreleased]
+
+#### Changed
+
+- The timer, session and settings now emit a payload object instead of a dictionary
+
 ### 0.9.0
 
 #### Changed
--- test_plugin.py
+++ test_plugin.py
@@ -1,81 +1,81 @@
 from unittest.mock import Mock, patch
 
 import pytest
+
 from tomate.config import Config
 from tomate.constant import Sessions, State
 from tomate.event import Events
 from tomate.graph import graph
-
-
-def method_called(result):
-    return result[0][0]
+from tomate.session import SessionPayload
 
 
 def setup_function(function):
     graph.providers.clear()
 
-    graph.register_instance('tomate.config', Mock(spec=Config))
+    graph.register_instance(
+        "tomate.config",
+        Mock(
+            spec=Config, **{"get_icon_path.return_value": "/path/to/mock/32/tomate.png"}
+        ),
+    )
 
     Events.Session.receivers.clear()
 
 
 @pytest.fixture()
-def plugin():
-    from notify_plugin import NotifyPlugin
+def plugin(mocker):
+    with mocker.patch("gi.repository.Notify.Notification.new"):
+        from notify_plugin import NotifyPlugin
 
-    return NotifyPlugin()
+        return NotifyPlugin()
 
 
-@patch('gi.repository.Notify.init')
-def test_init_dbus(init, plugin):
+def test_set_name_and_timeout_when_plugin_is_created(plugin):
+    from gi.repository import Notify
+
+    plugin.notification.set_timeout.assert_called_once_with(Notify.EXPIRES_NEVER)
+
+
+@patch("gi.repository.Notify.init")
+def test_enable_notify_when_plugin_active(init, plugin):
     plugin.activate()
 
-    init.assert_called_with('Tomate')
+    init.assert_called_with("Tomate")
 
 
-@patch('gi.repository.Notify.uninit')
-def test_uninit_dbus(uninit, plugin):
+@patch("gi.repository.Notify.uninit")
+def test_disable_notify_when_plugin_deactivate(uninit, plugin):
     plugin.deactivate()
 
     uninit.assert_called_with()
 
 
-def test_get_icon_path(plugin):
-    plugin.config.get_icon_path.return_value = '/path/to/mock/32/tomate.png'
-
-    assert plugin.icon_path == '/path/to/mock/32/tomate.png'
-
-
-@patch('gi.repository.Notify.Notification.new')
-def test_should_show_pomodoro_start_session_message(notification, plugin):
-    plugin.on_session_started()
-
-    title = plugin.messages['pomodoro']['title']
-    message = plugin.messages['pomodoro']['content']
-
-    notification.assert_called_once_with(title, message, plugin.icon_path)
-
-
-@patch('gi.repository.Notify.Notification.new')
-def test_should_show_session_finished_message(notification, plugin):
-    plugin.on_session_finished(task=Sessions.shortbreak)
-
-    notification.assert_called_once_with("The time is up!", '', plugin.icon_path)
-
-
-def test_should_call_on_session_finished_when_session_finished(plugin):
+def test_should_show_notification_when_session_starts(plugin):
     plugin.activate()
 
-    result = Events.Session.send(State.finished)
+    session_type = Sessions.pomodoro
 
-    assert len(result) == 1
-    assert plugin.on_session_finished == method_called(result)
+    payload = SessionPayload(
+        type=session_type, sessions=[], state=State.started, duration=0, task=0
+    )
+
+    Events.Session.send(State.started, payload=payload)
+
+    title = plugin.messages[session_type.name]["title"]
+    message = plugin.messages[session_type.name]["content"]
+
+    plugin.notification.update.assert_called_once_with(
+        title, message, "/path/to/mock/32/tomate.png"
+    )
+    plugin.notification.show.assert_called_once()
 
 
-def test_should_call_on_session_started_when_session_started(plugin):
+def test_should_show_notification_when_sessions_ends(plugin):
     plugin.activate()
 
-    result = Events.Session.send(State.started)
+    Events.Session.send(State.finished)
 
-    assert len(result) == 1
-    assert plugin.on_session_started == method_called(result)
+    plugin.notification.update.assert_called_once_with(
+        "The time is up!", "", "/path/to/mock/32/tomate.png"
+    )
+    plugin.notification.show.assert_called_once()
